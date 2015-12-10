3b95ec2bf192aff64f507e626d77debcf12c497c
Dinko Mihovilovic <dinko.mih@gmail.com>
2015-12-10 22:01:14 +0100

    Change comment

--- linover.js
+++ linover.js
@@ -3,6 +3,6 @@ if (window.location.href.indexOf('linkedin') !== -1) {
     document.getElementById('header-invitations-count').style.display = 'none';
   }
   catch (err) {
-    // We're either not on LinkedIn or there is no notification for removing
+    // There's nothing to remove
   }
 }
