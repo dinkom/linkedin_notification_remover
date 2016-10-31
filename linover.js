1e30f5e6b0fa230d1a82e2ebfd6f73ac89f2c534
Dinko Mihovilovic <dinko.mih@gmail.com>
2016-10-31 11:16:31 +0100

    Check domain from manifest, not the content script #1

--- linover.js
+++ linover.js
@@ -1,8 +1,6 @@
-if (window.location.href.indexOf('linkedin') !== -1) {
-  try {
-    document.getElementById('header-invitations-count').style.display = 'none';
-  }
-  catch (err) {
-    // There's nothing to remove
-  }
+try {
+	document.getElementById('header-invitations-count').style.display = 'none';
+}
+catch (err) {
+	// There's nothing to remove
 }
